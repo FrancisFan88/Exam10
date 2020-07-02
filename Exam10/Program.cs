57f4974d59fdc85f9ef9efed31d6ce0aa804e931
Francis <52584482+FrancisFan88@users.noreply.github.com>
2020-07-02 19:29:58 -0400

    First commit

---
+++ Exam10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exam10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class bank
+    {
+        public int MyProperty { get; set; }
+        public void doSomething()
+        { 
+        }
+    }
+}
