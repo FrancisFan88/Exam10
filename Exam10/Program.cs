c3cef7cb4ba2ee25461941188558e84900c50a8d
Francis <52584482+FrancisFan88@users.noreply.github.com>
2020-07-02 20:34:38 -0400

    commit2

--- Exam10/Program.cs
+++ Exam10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exam10
 {
@@ -7,13 +8,46 @@ namespace Exam10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Adult adult1 = new Adult("Talor",  "AA");
+            Child child1 = new Child("Child1", "BB");            
+            Adult[] persons = new Adult[2];
+            persons.Append(adult1);
+            persons.Append(child1);
+            foreach (person in persons)
+            {
+                Console.WriteLine(person.firstname, person.lastname);
+            }
         }
     }
-    public class bank
+    
+    public class Adult
     {
-        public int MyProperty { get; set; }
-        public void doSomething()
-        { 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Adult(string firstname, string lastname)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            Console.WriteLine("First Name:{0},Last Name: {1}", this.FirstName, this.LastName);
+        }
+        public Adult()
+        {
+            Console.WriteLine("First Name:{0},Last Name:{1}", this.FirstName, this.LastName);
+        }
+    }
+    public class Child
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Child(string firstname, string lastname)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            Console.WriteLine("First Name:{0},Last Name:{1}", this.FirstName, this.LastName);
+        }
+        public Child()
+        {
+            Console.WriteLine("First Name:{0},Last Name:{1}", this.FirstName, this.LastName);
         }
     }
 }
